ea54af7dabfac7de3494b02a3876e7c4b52219a5
unknown <ohndaniel@gmail.com>
2019-10-23 22:05:17 -0700

    Add project and basic layout.

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/App.css'
+
+function App() {
+  const getQuotes = () => {}
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <div className="tweet">
+        <div className="header">
+          <p>avatar</p>
+          <p>@kanye west</p>
+        </div>
+        <p>Quote Text Goes Here...</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
